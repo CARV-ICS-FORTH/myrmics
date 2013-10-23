a91ce3b6f492abaac2acb41cf9a4ea51a6fe377d
Foivos S. Zakkak <zakkak@ics.forth.gr>
2013-10-24 02:08:32 +0300

    Fix kt_strncmp

--- kt/string.c
+++ kt/string.c
@@ -1,21 +1,21 @@
 // ============================================================================
-// Copyright (c) 2013, FORTH-ICS / CARV 
+// Copyright (c) 2013, FORTH-ICS / CARV
 //                     (Foundation for Research & Technology -- Hellas,
 //                      Institute of Computer Science,
 //                      Computer Architecture & VLSI Systems Laboratory)
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // ==========================[ Static Information ]===========================
 //
 // Author        : Spyros Lyberis
@@ -214,7 +214,7 @@ int kt_strncmp(const char *s1, const char *s2, int num_bytes) {
   if (num_bytes == 0)
     return 0;
 
-  for ( ; *s1 == *s2 && num_bytes > 0; s1++, s2++, num_bytes--) {
+  for ( ; *s1 == *s2 && num_bytes > 1; s1++, s2++, num_bytes--) {
     if (!(*s1)) {
       return 0;
     }
